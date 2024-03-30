59fb7d5b462329c1882556ea2237d71e2218bbd8
justinkook <justinkook@gmail.com>
2024-03-30 15:39:36 -0400

    fix: add encoding and ignore pino pretty

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,6 +8,13 @@ const nextConfig = {
     if (!isServer) {
       config.resolve.fallback = { fs: false, net: false, tls: false }
     }
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
+        })
+      )
+    }
     return config
   },
 }
