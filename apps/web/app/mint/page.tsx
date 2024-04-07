cd4a801f84e33c638f69bf909c908803c72d7711
justinkook <justinkook@gmail.com>
2024-04-07 17:13:45 -0400

    fix: use getNftsForOwnerIterator

--- apps/web/app/mint/page.tsx
+++ apps/web/app/mint/page.tsx
@@ -70,12 +70,15 @@ export default function MintPage() {
       if (!address) {
         return
       }
-      const { ownedNfts } = await alchemy.nft.getNftsForOwner(address, {
+      const ownedNfts = await alchemy.nft.getNftsForOwnerIterator(address, {
         contractAddresses: [GALVERSE_CONTRACT_ADDRESS],
       })
-      const filteredNfts = ownedNfts.filter(
-        (e) => (e.image?.cachedUrl || e.image?.pngUrl) && e.mint?.mintAddress
-      )
+      const filteredNfts = []
+      for await (const nft of ownedNfts) {
+        if (nft.image?.cachedUrl || nft.image?.pngUrl) {
+          filteredNfts.push(nft)
+        }
+      }
       setNfts(filteredNfts)
     }
     getNFTs()
